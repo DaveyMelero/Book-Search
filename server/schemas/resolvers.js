4bb3dbd042963c7f426e6b7f1c4f69aa669cc6f2
Davey Melero <Davey1234576@gmail.com>
2023-04-23 14:07:05 -0700

    fix resolvers folder

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,120 +1,70 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
-const { signToken } = require('../utils/auth');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth')
 
+//resolvers create functions, logic for mutations
 const resolvers = {
-  Query: {
-    users: async () => {
-      return User.find().populate('thoughts');
+    Query: {
+        me: async(parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({_id: context.user._id})
+                    .select ('-__v -password');
+            return userData;
+            };
+            throw new AuthenticationError('Please login');
+        }
     },
-    user: async (parent, { username }) => {
-      return User.findOne({ username }).populate('thoughts');
+    Mutation: {
+        addUser: async(parent, {username, email, password}) => {
+            try {
+                const user = await User.create({username, email, password});
+                const token = signToken(user);
+                
+                return {token, user};
+            } catch (err) {
+                throw new Error ('Failed to add user');
+            }
+        },
+        login: async(parent,{email, password}) => {
+            try {
+            const user = await User.findOne({email});
+            if (!user) {
+                throw new AuthenticationError('Incorrect email or password');
+            }
+            const passwordCheck = await user.isCorrectPassword(password);
+            if (!passwordCheck) {
+                throw new AuthenticationError('Incorrect email or password');
+            }
+            const token = signToken(user);
+            return {token, user};
+        } catch (err) {
+            throw new AuthenticationError('Failed to login');
+        }
     },
-    thoughts: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Thought.find(params).sort({ createdAt: -1 });
+    saveBook: async(parent, {input}, context) => {
+        try {
+            if (context.user) {
+                const updatedUserData = await User.findByIdAndUpdate({_id: context.user._id}, {$addToSet: {savedBooks: input}}, {new: true, runValidators: true});
+                
+                return updatedUserData;
+            }
+            throw new AuthenticationError('Please log in to save book');
+        } catch (err) {
+            throw new Error ('Failed to save book');
+        }
     },
-    thought: async (parent, { thoughtId }) => {
-      return Thought.findOne({ _id: thoughtId });
-    },
-    me: async (parent, args, context) => {
-      if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate('thoughts');
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-  },
-
-  Mutation: {
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
-      const token = signToken(user);
-      return { token, user };
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        throw new AuthenticationError('No user found with this email address');
-      }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const token = signToken(user);
-
-      return { token, user };
-    },
-    addThought: async (parent, { thoughtText }, context) => {
-      if (context.user) {
-        const thought = await Thought.create({
-          thoughtText,
-          thoughtAuthor: context.user.username,
-        });
-
-        await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $addToSet: { thoughts: thought._id } }
-        );
-
-        return thought;
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    addComment: async (parent, { thoughtId, commentText }, context) => {
-      if (context.user) {
-        return Thought.findOneAndUpdate(
-          { _id: thoughtId },
-          {
-            $addToSet: {
-              comments: { commentText, commentAuthor: context.user.username },
-            },
-          },
-          {
-            new: true,
-            runValidators: true,
-          }
-        );
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    removeThought: async (parent, { thoughtId }, context) => {
-      if (context.user) {
-        const thought = await Thought.findOneAndDelete({
-          _id: thoughtId,
-          thoughtAuthor: context.user.username,
-        });
-
-        await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $pull: { thoughts: thought._id } }
-        );
-
-        return thought;
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    removeComment: async (parent, { thoughtId, commentId }, context) => {
-      if (context.user) {
-        return Thought.findOneAndUpdate(
-          { _id: thoughtId },
-          {
-            $pull: {
-              comments: {
-                _id: commentId,
-                commentAuthor: context.user.username,
-              },
-            },
-          },
-          { new: true }
-        );
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-  },
+    removeBook: async(parent, {bookId}, context) => {
+        try {
+            if (context.user) {
+                const  updatedUserData = await User.findOneAndUpdate({_id: context.user._id}, {$pull: {savedBooks: {bookId}}}, {new:true});
+                
+                return updatedUserData;
+            }
+            throw new AuthenticationError('Please log in to remove book');
+        } catch (err) {
+            throw new Error ('Failed to remove book');
+        }
+    }
+}
 };
-
 module.exports = resolvers;
