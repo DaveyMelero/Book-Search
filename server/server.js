e49809d28eec226f6d2bfc541d6ba86e152dbe99
Davey Melero <Davey1234576@gmail.com>
2023-04-27 10:09:28 -0700

    change port to 3001

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
